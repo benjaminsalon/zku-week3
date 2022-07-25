669973315166b841182230a2ec2a5a66bcde339e
benjaminsalon <benjaminsalon.business@gmail.com>
2022-07-25 23:51:46 +0200

    week3

--- Part1/contracts/circuits/MastermindVariation.circom
+++ Part1/contracts/circuits/MastermindVariation.circom
@@ -2,8 +2,103 @@ pragma circom 2.0.0;
 
 // [assignment] implement a variation of mastermind from https://en.wikipedia.org/wiki/Mastermind_(board_game)#Variation as a circuit
 
-template MastermindVariation() {
+include "../../node_modules/circomlib/circuits/poseidon.circom";
+include "../../node_modules/circomlib/circuits/comparators.circom";
 
+
+template MastermindVariation() {
+    // Let's do the Royale Mastermind -> 5 colors and 5 shapes, 3 holes
+    signal input color1;
+    signal input shape1;
+    signal input color2;
+    signal input shape2;
+    signal input color3;
+    signal input shape3;
+
+    signal input nbHits;
+    signal input nbBlows;
+
+    //The hash of the solution to avoid brute force
+    signal input hashPub;
+
+    signal input rightColor1;
+    signal input rightShape1;
+    signal input rightColor2;
+    signal input rightShape2;
+    signal input rightColor3;
+    signal input rightShape3;
+
+    signal input salt;
+
+    signal output pubSolOut;
+
+    // Add a constraint to verify if the input is sane
+    var colors[3] = [color1, color2, color3];
+    var shapes[3] = [shape1, shape2, shape3];
+    component lessThan[6];
+
+    var i = 0;
+    var j = 0;
+    
+    for (i=0; i<3; i++){
+        lessThan[2*i] = LessThan(4);
+        lessThan[2*i].in[0] <== colors[i];
+        lessThan[2*i].in[1] <== 5;
+        lessThan[2*i].out === 1;
+
+        lessThan[2*i+1] = LessThan(4);
+        lessThan[2*i+1].in[0] <== shapes[i];
+        lessThan[2*i+1].in[1] <== 5;
+        lessThan[2*i+1].out === 1;
+    }
+
+    // We will work on a grid of 5*5
+
+    var inputsComputed[3] = [color1*5+shape1, color2*5+shape2, color3*5+shape3];
+    var rightInputsComputed[3] = [rightColor1*5+rightShape1, rightColor2*5+rightShape2, rightColor3*5+rightShape3];
+
+
+    // Let's count the hits and blows (wrong vocabulary but easier than black and whites I find)
+    var nbHitsReal = 0;
+    var nbBlowsReal = 0;
+
+    component equals[9];
+    
+
+    for (i = 0; i <3; i++) {
+        for (j = 0; j<3; j++) {
+            equals[i*3+j] = IsEqual();
+            equals[i*3+j].in[0] <== inputsComputed[i];
+            equals[i*3+j].in[1] <== rightInputsComputed[j];
+            if (i==j) {
+                nbHitsReal += equals[i*3+j].out;
+            }
+            else {
+                nbBlowsReal += equals[i*3+j].out;
+            }    
+        }
+    }
+    // Check if the hits and blows are correct
+    component equalHit = IsEqual();
+    equalHit.in[0] <== nbHits;
+    equalHit.in[1] <== nbHitsReal;
+    equalHit.out === 1;
+
+    component equalBlow = IsEqual();
+    equalBlow.in[0] <== nbBlows;
+    equalBlow.in[1] <== nbBlowsReal;
+    equalBlow.out === 1;
+
+    // Verify the hash
+
+    component poseidon = Poseidon(4);
+    poseidon.inputs[0] <== salt;
+    poseidon.inputs[1] <== rightInputsComputed[0];
+    poseidon.inputs[2] <== rightInputsComputed[1];
+    poseidon.inputs[3] <== rightInputsComputed[2];
+
+    poseidon.out === hashPub;
+    pubSolOut <== poseidon.out;
 }
 
-component main = MastermindVariation();
+component main {public [color1,shape1,color2,shape2,color3,shape3,nbHits,nbBlows,hashPub]}= MastermindVariation();
--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,172 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+const { expect, assert } = require("chai");
+const { ethers } = require("hardhat");
+const { groth16} = require("snarkjs");
+
+const wasm_tester = require("circom_tester").wasm;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+const pubSol = 19088437073470657864708272409024615978633461548930206696950606552836135386003n; // Precomputed
+
+
+const inputSignals = {
+    "color1" : "1",
+    "shape1" : "1",
+    "color2" : "2",
+    "shape2" : "2",
+    "color3" : "3",
+    "shape3" : "3",
+    "nbHits" : "1",
+    "nbBlows" : "1",
+    "hashPub" : pubSol,
+    "rightColor1" : "1",
+    "rightShape1" : "1",
+    "rightColor2" : "4",
+    "rightShape2" : "4",
+    "rightColor3" : "2",
+    "rightShape3" : "2",
+    "salt" : "333"
+}
+
+const wrongInputSignals = {
+    "color1" : "1",
+    "shape1" : "1",
+    "color2" : "2",
+    "shape2" : "2",
+    "color3" : "3",
+    "shape3" : "3",
+    "nbHits" : "1",
+    "nbBlows" : "0",
+    "hashPub" : pubSol,
+    "rightColor1" : "1",
+    "rightShape1" : "1",
+    "rightColor2" : "4",
+    "rightShape2" : "4",
+    "rightColor3" : "2",
+    "rightShape3" : "2",
+    "salt" : "333"
+}
+
+const invalidInputSignals = {
+    "color1" : "1",
+    "shape1" : "1",
+    "color2" : "2",
+    "shape2" : "2",
+    "color3" : "3",
+    "shape3" : "5",
+    "nbHits" : "1",
+    "nbBlows" : "0",
+    "hashPub" : pubSol,
+    "rightColor1" : "1",
+    "rightShape1" : "1",
+    "rightColor2" : "4",
+    "rightShape2" : "4",
+    "rightColor3" : "2",
+    "rightShape3" : "2",
+    "salt" : "333"
+}
+
+const wrongSaltInputSignals = {
+    "color1" : "1",
+    "shape1" : "1",
+    "color2" : "2",
+    "shape2" : "2",
+    "color3" : "3",
+    "shape3" : "3",
+    "nbHits" : "1",
+    "nbBlows" : "1",
+    "hashPub" : pubSol,
+    "rightColor1" : "1",
+    "rightShape1" : "1",
+    "rightColor2" : "4",
+    "rightShape2" : "4",
+    "rightColor3" : "2",
+    "rightShape3" : "2",
+    "salt" : "33"
+}
+
+
+describe("MastermindVariation with Groth16", function () {
+
+    beforeEach(async function () {
+        //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
+
+        
+    });
+
+    it("Should return true for a correct combination of inputs", async function () {
+        //[assignment] insert your script here
+        
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+        const witness = await circuit.calculateWitness(inputSignals, true);
+    });
+
+    it("Should return false for invalid combination of inputs - Blows changed", async function () {
+        //[assignment] insert your script here
+
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+        try {
+            const witness = await circuit.calculateWitness(wrongInputSignals, true);
+        }
+        catch (e) {
+            expect(e.message).to.contain("line: 90"); // We modified the number of blows.
+        }
+    });
+
+    it("Should return false for invalid inputs - Shape indice not in range", async function () {
+        //[assignment] insert your script here
+
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+        try {
+            const witness = await circuit.calculateWitness(invalidInputSignals, true);
+        }
+        catch (e) {
+            expect(e.message).to.contain("line: 52"); // One shape indice is too high (superior to 4).
+        }
+    });
+
+    it("Should return false for invalid salt", async function () {
+        //[assignment] insert your script here
+
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+        try {
+            const witness = await circuit.calculateWitness(wrongSaltInputSignals, true);
+        }
+        catch (e) {
+            // console.log(e.message);
+            expect(e.message).to.contain("line: 100");
+        }
+    });
+
+    // Having issues with the full proof
+    // it("Do a full and valid proof", async function () {
+    //     //[assignment] insert your script here
+    //     const { proof, publicSignals } = await groth16.fullProve(inputSignals, "contracts/circuits/MastermindVariation_js/MastermindVariation.wasm","contracts/circuits/circuit_final.zkey");
+        
+    //     // We retrieve the parameterized calldata from the proof and signals to be able to call the smart contract verifier with the correct data.
+    //     const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
+    //     // console.log(calldata)
+    //     // Doing some formatting to get an array of strings from the array of arrays of bigNumbers that we have in calldata
+    //     const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+    //     // console.log(argv)
+    //     //The next lines are the creation of the correct parameters for the contract call
+    //     const a = [argv[0], argv[1]];
+    //     const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+    //     const c = [argv[6], argv[7]];
+    //     const Input = argv.slice(8);
+
+    //     // Contract call to verify the proof
+    //     expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+    // });
+});
